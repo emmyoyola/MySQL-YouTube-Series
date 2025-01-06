fd41f895f3a956e710a1396db7509196eb20e5b7
emmyoyola <emmyoyola@gmail.com>
2025-01-06 16:22:29 -0500

    Add files

---
+++ 2. Select Statement.sql
@@ -0,0 +1,20 @@
+-- SELECT STATEMENT
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name,
+last_name,
+birth_date,
+age,
+(age + 10) * 10 +10 AS operation
+FROM parks_and_recreation.employee_demographics;
+
+#PEMDAS P: Parentheses; E: Exponents; M: Multiplication; D: Division; A: Addition; S: Substraction
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
+
+
+
+ 
---
+++ 3. Where Clause.sql
@@ -0,0 +1,79 @@
+-- WHERE CLAUSE
+
+SELECT * 
+FROM parks_and_recreation.employee_salary
+WHERE first_name = 'Leslie';
+
+SELECT * 
+FROM parks_and_recreation.employee_salary
+WHERE salary > 50000;
+
+SELECT * 
+FROM parks_and_recreation.employee_salary
+WHERE salary >= 50000;
+
+SELECT * 
+FROM parks_and_recreation.employee_salary
+WHERE salary < 50000;
+
+SELECT * 
+FROM parks_and_recreation.employee_salary
+WHERE salary <= 50000;
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE gender != 'Female';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE birth_date > '1985-01-01';
+
+
+-- AND | OR | NOT -- Logical Operators
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE birth_date > '1985-01-01' 
+AND gender = 'male';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE birth_date > '1985-01-01' 
+OR gender = 'male';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE birth_date > '1985-01-01' 
+OR NOT gender = 'male';
+
+# Tambien aplica PEMDAS para logical operators
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44) OR gender = 'Male';
+
+
+-- LIKE Statement (% and _)
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE first_name LIKE 'Jer%';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE first_name LIKE '%a%';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE first_name LIKE 'a%';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE first_name LIKE 'a___';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE first_name LIKE 'a___%';
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE birth_date LIKE '1989%';
